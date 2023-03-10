3b852a8c92d4c0a35740051c7b16f2e861c87522
rohanimds <HP@DESKTOP-I86B4EQ>
2023-03-10 14:46:09 +0530

    commit

---
+++ src/test/java/hrms/New.java
@@ -0,0 +1,14 @@
+package hrms;
+
+import org.apache.commons.collections4.bag.SynchronizedSortedBag;
+import org.testng.annotations.Test;
+
+public class New {
+	@Test
+public void main() {
+	System.out.println("test1");
+}
+public void jain() {
+	System.out.println("test2");
+}
+}
