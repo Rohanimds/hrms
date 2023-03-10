aab083044a08a847fc0f12d3c6d78955fd571a0e
rohanimds <HP@DESKTOP-I86B4EQ>
2023-03-10 15:10:26 +0530

    go

--- src/test/java/hrms/New2Test.java
+++ src/test/java/hrms/New2Test.java
@@ -2,7 +2,7 @@ package hrms;
 
 import org.testng.annotations.Test;
 
-public class New2 {
+public class New2Test {
 	@Test
 public void up() {
 	System.out.println("test5");
--- src/test/java/hrms/NewTest.java
+++ src/test/java/hrms/NewTest.java
@@ -3,7 +3,7 @@ package hrms;
 import org.apache.commons.collections4.bag.SynchronizedSortedBag;
 import org.testng.annotations.Test;
 
-public class New {
+public class NewTest {
 	@Test
 public void main() {
 	System.out.println("test1");
